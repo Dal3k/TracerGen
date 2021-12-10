c707d8da7b2ad5ed5a5d932cbfa08595c8457462
G@t <gaetan.pusel@epita.fr>
2021-12-10 21:08:15 +0100

    using class in main

--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 #include <fstream>
 
+#include "color.h"
+#include "vec3.h"
+
 int main() {
     // Image
 
     const int image_width = 256;
     const int image_height = 256;
+
     std::ofstream myfile;
     myfile.open("image.ppm");
 
@@ -16,17 +20,11 @@ int main() {
     for (int j = image_height - 1; j >= 0; --j) {
         std::cout << "\rScanlines remaining: " << j << ' ' << std::flush;
         for (int i = 0; i < image_width; ++i) {
-            auto r = double(i) / (image_width - 1);
-            auto g = double(j) / (image_height - 1);
-            auto b = 0.25;
-
-            int ir = static_cast<int>(255.999 * r);
-            int ig = static_cast<int>(255.999 * g);
-            int ib = static_cast<int>(255.999 * b);
-
-            myfile << ir << ' ' << ig << ' ' << ib << '\n';
+            color pixel_color(double(i) / (image_width - 1), double(j) / (image_height - 1), 0.25);
+            write_color(std::cout, pixel_color);
         }
     }
+    std::cout << "\nDone\n";
     myfile.close();
     return 0;
 }
