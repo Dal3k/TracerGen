71fd7a8ec686db315172de7bfc3e1d9fd82bddce
G@t <gaetan.pusel@epita.fr>
2021-12-10 20:56:54 +0100

    vec3.h

--- main.cpp
+++ main.cpp
@@ -13,11 +13,11 @@ int main() {
 
     myfile << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
-    for (int j = image_height-1; j >= 0; --j) {
+    for (int j = image_height - 1; j >= 0; --j) {
         std::cout << "\rScanlines remaining: " << j << ' ' << std::flush;
         for (int i = 0; i < image_width; ++i) {
-            auto r = double(i) / (image_width-1);
-            auto g = double(j) / (image_height-1);
+            auto r = double(i) / (image_width - 1);
+            auto g = double(j) / (image_height - 1);
             auto b = 0.25;
 
             int ir = static_cast<int>(255.999 * r);
