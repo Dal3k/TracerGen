724a2daf076f425c9583183188ac03929849f671
G@t <gaetan.pusel@epita.fr>
2021-12-15 15:50:20 +0100

    Motion blur

--- main.cpp
+++ main.cpp
@@ -8,6 +8,8 @@
 #include "sphere.h"
 #include "camera.h"
 #include "material.h"
+#include "moving_sphere.h"
+
 
 color ray_color(const ray &r, const hittable &world, int depth) {
     hit_record rec;
@@ -40,18 +42,21 @@ hittable_list random_scene() {
             auto choose_mat = random_double();
             point3 center(a + 0.9 * random_double(), 0.2, b + 0.9 * random_double());
 
-            if ((center - point3(4, 0.2, 0)).length() > 0.9) {
+            if ((center - vec3(4, 0.2, 0)).length() > 0.9) {
                 shared_ptr<material> sphere_material;
 
                 if (choose_mat < 0.8) {
                     // diffuse
-                    auto albedo = color(random_double(), random_double(), random_double());
+                    auto albedo = color(random_double(), random_double(), random_double()) *
+                            color(random_double(), random_double(), random_double());
                     sphere_material = make_shared<lambertian>(albedo);
-                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
+                    auto center2 = center + vec3(0, random_double(0, .5), 0);
+                    world.add(make_shared<moving_sphere>(
+                            center, center2, 0.0, 1.0, 0.2, sphere_material));
                 } else if (choose_mat < 0.95) {
                     // metal
                     auto albedo = color(random_double(), random_double(), random_double());;
-                    auto fuzz = random_double(0, 0.5);
+                    auto fuzz = random_double(0.1, 0.7);
                     sphere_material = make_shared<metal>(albedo, fuzz);
                     world.add(make_shared<sphere>(center, 0.2, sphere_material));
                 } else {
@@ -79,10 +84,10 @@ hittable_list random_scene() {
 int main() {
     // Image
 
-    const auto aspect_ratio = 3.0 / 2.0;
-    const int image_width = 400;
-    const int image_height = static_cast<int>(image_width / aspect_ratio);
-    const int samples_per_pixel = 30;
+    const auto aspect_ratio = 16 / 9;
+    const int image_height = 480;
+    const int image_width = static_cast<int>(image_height * aspect_ratio);
+    const int samples_per_pixel = 20;
     const int max_depth = 5;
     std::ofstream myfile;
     myfile.open("image.ppm");
